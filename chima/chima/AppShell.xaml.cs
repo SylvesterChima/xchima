d6387bdd001a021437d6e9ca15021e59d4a2f540
Sylvester Eze <seze@eminenttechnology.com>
2020-11-20 10:09:26 +0100

    Add project files.

---
+++ chima/chima/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using chima.ViewModels;
+using chima.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace chima
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
